2523a9fc3fb232e5f7d0a8cc1a1a8c7131b003b7
arie900 <arie902@gmail.com>
2020-07-10 21:57:29 +0300

    Update CreateGroup.ps1

--- CreateGroup.ps1
+++ CreateGroup.ps1
@@ -1,7 +1,6 @@
-﻿$GroupName = "Management"               #Read-Host "Write group name"
-#$GroupDesciption = Read-Host "Write description of the group"
+﻿$GroupName = Read-Host "Write ADGroupName"
+$GroupDesciption = Read-Host "Write description of the group"
 
-
+#Create new ADGroup in required path
 New-ADGroup -Name $GroupName -SamAccountName $GroupName `
--GroupCategory Security -GroupScope Global `
- -Path "CN=Users,DC=DNA,DC=com" #-Description $GroupDesciption
+-GroupCategory Security -GroupScope Global -Path "CN=Users,DC=DNA,DC=com" #-Description $GroupDesciption
