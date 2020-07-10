64de0f587654429fedd6908de466859ddbf22478
arie900 <arie902@gmail.com>
2020-07-10 23:09:31 +0300

    Add files via upload

---
+++ NewCreateUser.ps1
@@ -0,0 +1,37 @@
+﻿#Variables
+$Name=Read-Host "Write user first name"
+$LastName=Read-Host "Write user last name"
+$TZ = Read-Host "Enter ID"
+$UserName= ($Name+$LastName[0])
+$ExistUserName=Get-ADUser -Filter "SamAccountName -like '$UserName'"
+$ExistTZ = Get-ADUser -Filter "UserPrincipalName -like '$TZ@Arelion.local'" 
+
+#Check in look if there already user with the same UPN/SamAccountName
+do{
+    if($ExistTZ -ne $null){echo $UserName
+        echo "There a user with the same ID"
+        break
+    }
+    elseif($ExistUserName -ne $null){echo "User already exist $UserName"
+        $UserName=Read-Host "Enter user name"
+        $ExistUserName=Get-ADUser -Filter "SamAccountName -eq '$UserName'"}}
+while($ExistUserName -ne $null){
+    echo "User already exist $UserName"
+    $UserName=Read-Host "Enter user name"
+    $ExistUserName=Get-ADUser -Filter "SamAccountName -eq '$UserName'"}
+
+#If there no user with same UPN\SamAccountName the user will be created in required path
+New-ADUser  -Name $UserName -GivenName $Name `
+-Surname $LastName `
+-UserPrincipalName "$TZ@Arelion.local" -Enabled $true `
+-DisplayName "$Name $LastName" -Path "CN=Users,DC=Arelion,DC=local" `
+-AccountPassword (ConvertTo-SecureString "!q2w3e4r" -AsPlainText -Force) `
+-PasswordNeverExpires $true               
+            
+
+#$ExistUserName=Get-ADUser -Filter "SamAccountName -eq '$UserName'"
+#echo $ExistUserName
+#}while($if -eq $null)
+
+
+
