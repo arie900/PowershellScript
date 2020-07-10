91cb8d1791f66c5a0832f770e8c61e99b5bb53b5
arie900 <arie902@gmail.com>
2020-07-10 21:54:47 +0300

    Update CreateComputer.ps1

--- CreateComputer.ps1
+++ CreateComputer.ps1
@@ -1,2 +1,3 @@
-﻿$ComputerName = Read-Host "Enter computer name"
+#Insert computer name and create ADComputer to required path
+$ComputerName = Read-Host "Enter computer name"
 New-ADComputer -Name $ComputerName -UserPrincipalName $ComputerName -Path "CN=Computers,DC=DNA,DC=com"
