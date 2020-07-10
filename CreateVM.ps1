572d001c77cd2747c55503d9d6f1a80bc10f056a
arie900 <arie902@gmail.com>
2020-07-10 22:02:35 +0300

    Update CreateVM.ps1

--- CreateVM.ps1
+++ CreateVM.ps1
@@ -1,9 +1,14 @@
-﻿$VMName = "Arie-SVR1" #Insert VMName
+#Create enviroments
+$VMName = "Arie-SVR1" #Insert VMName
 $SwitchName = "ArelionSwitch"
 $Path = "C:\VM\$VMName"
 $localCred = Get-Credential -UserName administrator -Message "Local Machine Credential"
 $domainCred = Get-Credential -UserName Arelion\administrator -Message "Domain Credential"
+$IPAddres = "172.16.0.20"
+$DG = "172.16.0.1"
+$DNSServer = "172.16.0.10"
 
+#Create VM with required settings
 New-VM -Name $VMName -MemoryStartupBytes 2GB -NoVHD -SwitchName $SwitchName -Path $Path `
 -Generation 2 
 Copy-Item -Path C:\VMS\BASE\WindowsServer2016\WS2016Base.vhdx -Destination $Path\$VMName.vhdx
@@ -11,14 +16,16 @@ Add-VMHardDiskDrive -VMName $VMName -Path $Path\$VMName.vhdx -ControllerType SCS
 $HD = Get-VMHardDiskDrive -VMName $VMName -ControllerLocation 1 -ControllerNumber 0 -ControllerType SCSI
 Set-VMFirmware -VMName $VMName -FirstBootDevice $hd
 
+#Start VM and set net adapter
 Start-VM -VMName $VMName
 Start-Sleep -Seconds 90
-#Invoke-Command -VMName $VMName -Credential $localCred -ArgumentList $domainCred,$VMName,$localCred{
-#param($domainCred,$VMName,$localCred)
-#get-NetAdapter -Name "Ethernet" | New-NetIPAddress -IPAddress 172.16.0.19 -PrefixLength 24 -DefaultGateway 172.16.0.1
-#Get-NetAdapter -Name "Ethernet" | Set-DnsClientServerAddress -ServerAddresses 172.16.0.10
-#}
-#Start-Sleep -Seconds 5
+Invoke-Command -VMName $VMName -Credential $localCred -ArgumentList $domainCred,$VMName,$localCred{
+param($domainCred,$VMName,$localCred)
+get-NetAdapter -Name "Ethernet" | New-NetIPAddress -IPAddress $IPAddres -PrefixLength 24 -DefaultGateway $DG
+Get-NetAdapter -Name "Ethernet" | Set-DnsClientServerAddress -ServerAddresses $DNSServer
+}
+#Add computer to DC
+Start-Sleep -Seconds 5
 Invoke-Command -VMName $VMName -Credential $localCred -ArgumentList $domainCred,$VMName,$localCred{
 param($domainCred,$VMName,$localCred)
 Add-Computer -DomainName Arelion.local -Credential $domainCred -NewName $VMName
